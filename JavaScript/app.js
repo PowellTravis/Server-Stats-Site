8fef26bae9ac4f219612e6c336a82553dfc2bb2d
Travis <tp523130@gmail.com>
2018-07-26 20:23:12 -0700

    Fetch method for Insta API Started

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,26 @@
 console.log("Hello World!");
 
-$(document).ready(function(){
+function onSuccess(res){
+  console.log('AJAX: ' + res.message + ',' + res.status);
+}
+function onError(res){
+  console.log('AJAX Error:' + res);
+}
 
-$.ajax
+var token = '5867414966.1677ed0.5bdc97a97b48463d81498d94fc229e89';
+var clientID = 'c411debdbee84d5fa853b391af6ca043';
+var UserID = '5867414966';
+
+fetch('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=' + token, {
+method: 'GET',
+headers: {
+  'Content-Type': 'text/plain'
+}
+}).then(res => res.json())
+.catch(error => console.error('Error:', error))
+.then(response => console.log('Success:', response));
+
+
+$(document).ready(function(){
 
 }) //Close document.ready
