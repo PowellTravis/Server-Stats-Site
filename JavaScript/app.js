2c63b0738cf983b806ce945f7f202a9307a944cb
Travis <tp523130@gmail.com>
2018-07-27 12:11:47 -0700

    Still Hating the GraphQL.

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -11,27 +11,25 @@ var token = '5867414966.1677ed0.5bdc97a97b48463d81498d94fc229e89';
 var clientID = 'c411debdbee84d5fa853b391af6ca043';
 var UserID = '5867414966';
 
-fetch('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=' + token, {
+fetch('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + token, {
 method: 'GET',
 headers: {
   'Content-Type': 'text/plain'
-}
+  }
+
 }).then(res => res.json())
 .catch(error => console.error('Error:', error))
-.then(response => console.log('Success:', response));
+.then(response => console.log('What the HELL GraphQL!?:', response.data["0"].images.standard_resolution));
 
-const hero = {
-  name: 'R2-D2',
-  apearsIn: 'Rise of The Sith'
-}
-
-{
-  hero {
-    name
-    appearsIn
-  }
-}
-
-$(document).ready(function(){
-
-}) //Close document.ready
+//
+// console.log(response);
+//
+// var response = res.json();
+//
+// $(document).ready(function(){
+//
+//
+//
+//   $('.instafeed').attr('src', response.data["0"].images.standard_resolution.url );
+//
+// }) //Close document.ready
